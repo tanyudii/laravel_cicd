6985fced217a8b2e3027ddfa26bf0142a6701e79
tanyudie <yudi@vodea.id>
2020-09-06 21:54:23 +0700

    setup cicd

---
+++ deploy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'recipe/rsync.php';
+
+set('application', 'My App');
+set('ssh_multiplexing', true);
+
+set('rsync_src', function () {
+    return __DIR__;
+});
+
+
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/storage/',
+        '/vendor/',
+        '/node_modules/',
+        '.github',
+        'deploy.php',
+    ],
+]);
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+host('cicd.staging-dev.site')
+    ->hostname('34.101.111.235')
+    ->stage('production')
+    ->user('root')
+    ->set('deploy_path', '/var/www/html/cicd.staging-dev.site');
+
+host('cicd-dev.staging-dev.site')
+    ->hostname('34.101.111.235')
+    ->stage('staging')
+    ->user('root')
+    ->set('deploy_path', '/var/www/html/cicd-dev.staging-dev.site');
+
+after('deploy:failed', 'deploy:unlock');
+
+desc('Deploy the application');
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:secrets',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'artisan:queue:restart',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
