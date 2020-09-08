1fd182010a480da6ba435a4898ce707b491f8132
tanyudie <yudi@vodea.id>
2020-09-09 00:34:01 +0700

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Staging CICD
+                    Yudi
                 </div>
 
                 <div class="links">
