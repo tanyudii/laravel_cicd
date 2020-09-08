b3e05514e826d1519a8c38777aa4216323b2e50b
tanyudie <yudi@vodea.id>
2020-09-09 00:27:03 +0700

    upload staging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel CICD
+                    Laravel Staging CICD
                 </div>
 
                 <div class="links">
