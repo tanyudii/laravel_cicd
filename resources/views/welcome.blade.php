f8a09d530450704d96296273187be7a0220e2985
Yudi Hertanto <39409106+tanyudii@users.noreply.github.com>
2020-09-09 18:16:44 +0700

    Update welcome.blade.php
    update tiitle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Production
+                    Laravel Yudi
                 </div>
             </div>
         </div>
